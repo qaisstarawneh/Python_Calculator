3d1566ffd293823d31f9d2e1663413447e9e1365
qaisstarawneh <121809992+qaisstarawneh@users.noreply.github.com>
2023-03-07 01:56:07 +0300

    Add files via upload

---
+++ Calculator_file_task.py
@@ -0,0 +1,35 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+with open('input_file.txt', 'r') as file:
+    for line in file:
+        input_list = line.split()
+
+        if len(input_list) != 3:
+            print("Invalid input ")
+            continue
+
+        try:
+            x = int(input_list[0])
+            y = int(input_list[2])
+        except ValueError:
+            print("Invalid input ")
+            continue
+
+        if input_list[1] == '+':
+            print(x, "+", y, "=", add(x, y))
+        elif input_list[1] == '-':
+            print(x, "-", y, "=", subtract(x, y))
+        elif input_list[1] == '*':
+            print(x, "*", y, "=", multiply(x, y))
+        else:
+            print("Invalid input.")
